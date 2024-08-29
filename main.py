24b906763e507253cc43dcc647809ac30e951e61
B_Roosa <deman77777@gmail.com>
2024-08-29 18:24:22 -0400

    E: added player obj

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 def main():
     pygame.init()
     clock =pygame.time.Clock()
@@ -10,13 +11,17 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player_obj = Player(x= SCREEN_WIDTH/2, y= SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(000)
+        player_obj.draw(screen)
         pygame.display.flip()
+        
         dt = (clock.tick(FRAME_RATE))/1000 
         
 if __name__ == "__main__":
